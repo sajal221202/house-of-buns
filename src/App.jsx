58c2114baf1be97333d23c15abc6f95630849842
Sajal_G <sajal_g@Sajals-MacBook.local>
2026-09-15 01:21:56 +0530

    checkpoint: auto-save 2026-09-15 01:21:56

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import TopBar from './components/TopBar'
 import Nav from './components/Nav'
 import Hero from './components/Hero'
-import OpeningOffer from './components/OpeningOffer'
 import FeelTheHype from './components/FeelTheHype'
 import ArtisanalStack from './components/ArtisanalStack'
 import FeaturedBurgers from './components/FeaturedBurgers'
